710c63b86b1aac66fc7f29c36ab58e727e44721b
Victor Barreto <112266976+V1ct0rSb@users.noreply.github.com>
2024-08-21 15:22:46 -0300

    Banner da home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,29 @@
+import bannerVideo from "../../assets/Banner.mp4"
+import styles from "./Home.module.css"
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <main>
+      <section>
+        <div className={styles.bannerContainer}>
+          <video autoPlay loop muted className={styles.bannerVideo}>
+            <source src={bannerVideo} type="video/mp4" />
+            {/* Fallback para navegadores que não suportam vídeo */}
+            Seu navegador não suporta vídeos HTML5.
+          </video>
+          <div className={styles.bannerContent}>
+            <h1>
+              Seu bem-estar nas suas mãos. <br />
+              Registre seus sintomas e os compartilhe com seu médico.
+            </h1>
+            <button>Saiba mais</button>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <h1>teste</h1>
+      </section>
+    </main>
   )
 }
