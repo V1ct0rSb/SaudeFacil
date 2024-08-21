12d4f42ded538668494f16fa98645d0f6ebe7ed7
Victor Barreto <112266976+V1ct0rSb@users.noreply.github.com>
2024-08-21 11:54:19 -0300

    Header com responsividade

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,34 @@
+import { MdAccountCircle } from "react-icons/md"
 import { Link } from "react-router-dom"
+import styles from "./Navbar.module.css"
+
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-      </ul>
-    </nav>
+    <header className={styles.headerContainer}>
+      <div className={styles.logoHeader}>
+        <Link to="/" className={styles.logoLinkHeader}>
+          <img src="../src/assets/LG-suadeFacilTransparente.png" alt="" />
+        </Link>
+      </div>
+
+      <nav className={styles.navLinksContainer}>
+        <Link to="/sintomas" className={styles.navLink}>
+          Sintomas
+        </Link>
+        <Link to="/sobre" className={styles.navLink}>
+          Sobre
+        </Link>
+        <Link to="/contato" className={styles.navLink}>
+          Contato
+        </Link>
+      </nav>
+      
+      <div className={styles.loginIconHeader}>
+        <Link to="/">
+          <MdAccountCircle className={styles.loginIcon} />
+        </Link>
+      </div>
+    </header>
   )
 }
 
